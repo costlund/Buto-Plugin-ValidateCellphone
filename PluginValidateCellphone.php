0763c18ca3c549f1461c703b5cf93ccd07e6bbd5
Claes Ö <>
2021-04-27 11:36:56 +0200

    1.1.0

--- PluginValidateCellphone.php
+++ PluginValidateCellphone.php
@@ -1,5 +1,11 @@
 <?php
 class PluginValidateCellphone{
+  private $i18n = null;
+  function __construct() {
+    wfPlugin::includeonce('i18n/translate_v1');
+    $this->i18n = new PluginI18nTranslate_v1();
+    $this->i18n->setPath('/plugin/validate/cellphone/i18n');
+  }
   public function validate_cellphone($field, $form, $data = array()){
     if(wfArray::get($form, "items/$field/is_valid") && strlen(wfArray::get($form, "items/$field/post_value"))){
       $error = false;
@@ -15,17 +21,37 @@ class PluginValidateCellphone{
       if(!is_numeric(wfArray::get($form, "items/$field/post_value"))){
         $error = true;
       }
-      /**
-       * Starts with 07.
-       */
-      if(substr(wfArray::get($form, "items/$field/post_value"), 0, 2) != '07'){
-        $error = true;
-      }
       if($error){
         $form = wfArray::set($form, "items/$field/is_valid", false);
-        $form = wfArray::set($form, "items/$field/errors/", __('?label must be in format 07xxxxxxxx (ten digits)!', array('?label' => wfArray::get($form, "items/$field/label"))));
+        $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label must be numeric and have ten digits!', array('?label' => wfArray::get($form, "items/$field/label"))));
       }
     }
     return $form;
+  }
+  public function validate_country_code($field, $form, $data = array()){
+    if(wfArray::get($form, "items/$field/is_valid") && strlen(wfArray::get($form, "items/$field/post_value"))){
+      wfPlugin::includeonce('wf/array');
+      $data = new PluginWfArray($data);
+      $min = 10;
+      $max = 999;
+      if (!$this->is_integer(wfArray::get($form, "items/$field/post_value"))) {
+        $form = wfArray::set($form, "items/$field/is_valid", false);
+        $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label is not an integer!', array('?label' => wfArray::get($form, "items/$field/label"))));
+      }elseif((int)wfArray::get($form, "items/$field/post_value") < $min){
+        $form = wfArray::set($form, "items/$field/is_valid", false);
+        $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label can not be less then ?min!', array('?label' => wfArray::get($form, "items/$field/label"), '?min' => $min)));
+      }elseif((int)wfArray::get($form, "items/$field/post_value") > $max){
+        $form = wfArray::set($form, "items/$field/is_valid", false);
+        $form = wfArray::set($form, "items/$field/errors/", $this->i18n->translateFromTheme('?label can not be greater then ?max!', array('?label' => wfArray::get($form, "items/$field/label"), '?max' => $max)));
+      }
+    }
+    return $form;   
+  }
+  public function is_integer($num){
+    if(preg_match('/^\d+$/',str_replace('-', '', $num))){
+      return true;
+    }else{
+      return false;
+    }
   }
 }
--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # Buto-Plugin-ValidateCellphone
-
-Form validator to validate cellphone (swedish).
-
-Chell phone number must be in format 07xxxxxxxx.
-
-
+Form validator to validate cellphone and/or country code.
+Cellphone must have only numbers and ten digits.
+Country code muste have only numbers and between 10 and 999.
 ```
 items:
   phone:
@@ -15,5 +12,12 @@ items:
       -
         plugin: validate/cellphone
         method: validate_cellphone
+  country:
+    type: varchar
+    label: Country code
+    mandatory: true
+    validator:
+      -
+        plugin: validate/cellphone
+        method: validate_country_code
 ```
-
